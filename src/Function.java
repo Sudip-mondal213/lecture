9451a65130327b3b41995bbffd46d69bb5eb00e7
Sudip Mondal <msudip.mondal213gmail.com>
2025-05-16 07:47:29 +0530

    add some code

--- src/Function.java
+++ src/Function.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class Function {
+    public static void main(String[] args) {
+         greeting();
+    }
+    public static void greeting(){
+        System.out.println("good morning from sudip");
+    }
+
 }
